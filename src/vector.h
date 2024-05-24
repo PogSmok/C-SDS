b88eea69b8ee3287c31e36d0b680d689ff3d3111
Pog Smok <lukaszprus.job@gmail.com>
2024-05-24 17:19:38 +0200

    Refactored vector structure, removed docs

--- src/vector.h
+++ src/vector.h
@@ -14,179 +14,195 @@ limitations under the License.
 
 #pragma once
 
-#include <stdlib.h>
-#include <string.h>
+#ifndef v_stdlib
+#define v_stdlib
+#include <stdlib.h> // malloc() free()
+#endif // #ifndef v_stdlib
 
-// Using any of the functions below with different than expected datatypes (see comments and documentation) will result in an undefined behaviour.
+#ifndef v_string
+#define v_string
+#include <string.h> // memcpy() memmove()
+#endif // #ifndef v_string
 
-// T* at -> Array of type T, containing elements of the array.
-// size_t elementSize -> Size in bytes of an element stored in at, equivilent to sizeof(T).
-// size_t size -> Amount of elements stored in vector (at), index of last element is at[size-1].
-// size_t capacity -> Number of currently allocated elements (vec(t).at), once exceeded vec(T).at must be reallocated to greater size.
-#ifndef vec
-#define vec(T) \
-    struct { \
-        T* at; \
-        size_t elementSize; \
-        size_t size; \
-        size_t capacity; \
-    }
+/**
+ * Helper struct for storing information about the vector, it is stored at vector[-1]
+ * @private
+ */
+#define Vector_meta_data struct { size_t size; size_t capacity; }
+#define VECTOR_META_SIZE sizeof(Vector_meta_data)
 
-#endif // #ifndef vec
+/**
+ * Standardization of the syntax for definition of a vector
+ * @param {Type} T
+ */
+#define vector(T) T*
 
-// Input: 
-//      T -> Type of the variable to create the vector for.
-// Output:
-//      vec(T) -> The created vector.
-// Description:
-//      Constructor of vec(T).
-//      vec(int) myVector = new_vec(int);
-// Behaviour:
-//      1) If malloc fails vec(T).at will be a NULL pointer.
-//      2) By default capacity of a vec(T) is 32.
-#ifndef new_vec
-#define new_vec(T) \
-    { \
-        malloc(sizeof(T)*32), \
-        sizeof(T), \
-        0, \
-        32, \
-    }
+/**
+ * Returns memory adress of vector's meta data
+ * @private
+ */
+#define v_meta(vector) \
+    ((Vector_meta_data*)(vector-VECTOR_META_SIZE))
 
-#endif // #ifndef new_vec
+/**
+ * Returns number of bytes vector allocates, including meta data
+ * @param {vector} vector
+ * @return {size_t}
+ * @private
+ */
+#define v_raw_byte_size(vector) \
+    v_capacity(vector)*sizeof(*vector)+VECTOR_META_SIZE
 
-// Input:
-//      vec(T) vec
-// Output:
-//      void -> Function works in-place.
-// Description:
-//      Deallocates at of the vector and sets all values to 0.
-//      free({2,0,9}) -> {}
-// Behaviour:
-//      1) vec.at is explicitly set to NULL, thus all unsaved data is irreversably lost.
-#ifndef vec_free
-#define vec_free(vec) \
-    free(vec.at); \
-    vec.at = NULL; \
-    vec.elementSize = 0; \
-    vec.size = 0; \
-    vec.capacity = 0;
+/**
+ * Destructs a vector
+ * @param {vector} vector
+ */
+#define v_free(vector) \
+    free(v_meta(vector))
 
-#endif // #ifndef vec_free
+/**
+ * Returns the number of elements in the vector.
+ * @param {vector} vector
+ * @return size_t
+ */
+#define v_size(vector) \
+    (vector ? v_meta(vector)->size : 0)
 
-// Input:
-//      vec(T) vec
-//      size_t newSize -> New capacity of vector, amount of elements (not bytes!).
-// Output:
-//      void -> Function works in-place.
-// Description:
-//      Changes capacity of the vector to a desired value.
-//      {1,4,-2}.capacity = 32, vec_resize({1,4,-2}, 100) -> {1,4,-2}.capacity = 100
-// Behaviour:
-//      1) If realloc fails nothing is done.
-//      2) If the new capacity is smaller than vector's size, excessive elements will be lost and vector's size will be set to vector's capacity.
-#ifndef vec_resize
-#define vec_resize(vec, newSize) \
-    do { \
-        void* p = realloc(vec.at, newSize*vec.elementSize); \
-        if (p != NULL) { \
-            vec.capacity = newSize; \
-            vec.at = p; \
-            if(vec.capacity <= vec.size) vec.size = vec.capacity; \
-        } \
-    } while(0) 
+/**
+ * Helper function for exponential growth
+ * @param {vector}
+ */
+#define DEFAULT_VECTOR_CAPACITY 32
+#define v_grow(vector)                                                                                     \
+    do {                                                                                                   \
+        void* p = NULL;                                                                                    \
+        if(v_capacity(vector)) p = realloc(vector-VECTOR_META_SIZE, v_meta(vector)->capacity*(*vector)*2); \
+        else p = malloc(DEFAULT_VECTOR_CAPACITY*sizeof(*vector)+VECTOR_META_SIZE);                         \
+        if(p != NULL) {                                                                                    \
+            vector = p+VECTOR_META_SIZE;                                                                   \
+            size_t capacity = v_meta(vector)->capacity;                                                    \
+            v_meta(vector)->capacity = capacity ? capacity<<1 : DEFAULT_VECTOR_CAPACITY;                   \
+        }                                                                                                  \
+    }  while(0)
 
-#endif // #ifndef vec_resize
-
-// Input:
-//      vec(T) vec
-//      T element -> Element to be pushed to the vector.
-// Output:
-//      void -> Function works in-place.
-// Description: 
-//      Increments vector's size by 1 and appends provided element. Reallocates and increases capacity if needed.
-//      Uses memcpy to avoid type assertion for nested vectors.
-//      vec_push({13,9,5}, 3) -> {13,9,5,3} 
-// Behaviour:
-//      1) If reallocation is needed to fit the element and realloc fails nothing is done.
-#ifndef vec_push
-#define vec_push(vec, element) \
-    do { \
-        typeof(element) e = element; \
-        size_t lastCapacity = 0; \
-        if (vec.size >= vec.capacity) { \
-            lastCapacity = vec.capacity; \
-            vec_resize(vec, vec.capacity*2); \
-        } \
-        if (lastCapacity == 0 || lastCapacity < vec.capacity) \
-          memcpy(&vec.at[vec.size++], &e, vec.elementSize); \
-    } while(0)
-#endif // #ifndef vec_push
-
-// Input:
-//      vec(T) vec
-// Output:
-//      T -> the removed element
-// Description:
-//      Removes the last element of the vector and returns it.
-//      vec_pop({4,-1,25,31}) -> 31
-// Behaviour:
-//      1) If the vector has size of 0, vec.at[0] is returned.
-#ifndef vec_pop
-#define vec_pop(vec) \
-    vec.at[vec.size ? --vec.size : 0]
-
-#endif // #ifndef vec_pop
-
-// Input:
-//      vec(T) vec
-//      T element -> Element to be pushed to the vector.
-//      size_t index -> Index the element is to be inserted at.
-// Output:
-//      void -> Function works in-place.
-// Description: 
-//      Increments vector's size by 1 and inserts given elementa at the given index, shifting all bigger indexes one to the right.
-//      Uses memcpy for asigning element at index to avoid type assertion for nested vectors.
-//      vec_insert({2,6,-24,1}, -9, 1) -> {2,-9,6,-24,1}
-// Behaviour:
-//      1) If index is out of bounds, works as vec_push
-#ifndef vec_insert
-#define vec_insert(vec, element, index) \
-    do { \
-        if (index >= vec.size) vec_push(vec, element); \
-        else { \
-            typeof(element) e = element; \
-            size_t lastCapacity = 0; \
-            if (vec.size >= vec.capacity) { \
-                lastCapacity = vec.capacity; \
-                vec_resize(vec, vec.capacity*2); \
-            } \
-            if (lastCapacity == 0 || lastCapacity < vec.capacity) { \
-                memcpy(&vec.at[index+1], &vec.at[index], (vec.size-index)*vec.elementSize); \
-                memcpy(&vec.at[index], &e, vec.elementSize); \
-                vec.size++; \
-            } \
-        } \
+/**
+ * Resizes the container so that it contains n elements
+ * @param {vector} vector
+ * @param {size_t} n
+ */
+#define v_resize(vector, n)                                            \
+    do {                                                               \
+        void* old_ptr = vector;                                        \
+        do v_grow(vector);                                             \
+        while((n) > v_capacity(vector) && (*vector)old_ptr != vector); \
     } while(0)
 
-#endif // #ifndef vec_insert
+/**
+ * Returns the size of the storage space currently allocated for the vector, expressed in terms of elements.
+ * @param {vector} vector
+ * @return size_t
+ */
+#define v_capacity(vector) \
+    (vector ? v_meta(vector)->capacity : 0)
 
-// Input:
-//      vec(T) vec
-//      T index -> Index of the element that is to be removed.
-// Output:
-//      void -> Function works in-place.
-// Description:
-//      Removes element at the given index. Shifts all elements after the index by one to the left ([i]th index becomes [i-1]th index).
-//      vec_remove({1,2,12,6,-4}, 2) -> {1,2,6,-4}
-// Behaviour:
-//      1) If index is out of bounds, nothing is done.
-#ifndef vec_remove
-#define vec_remove(vec, index) \
-    if (index >= vec.size) vec_pop(vec); \
-    else { \
-        memcpy(&vec.at[index], &vec.at[index+1], (vec.size-index-1)*vec.elementSize); \
-        vec.size--; \
-    }
+/**
+ * Checks whether there are any elements in vector
+ * @param {vector} vector
+ * @return boolean 
+ */
+#define v_empty(vector) \
+    (!vector || (v_meta(vector)->size == 0))
 
-#endif // #ifndef vec_remove
+/**
+ * Requests that the vector capacity be at least enough to contain n elements.
+ * @param {vector} vector
+ * @param {size_t} n
+ */
+#define v_reserve(vector, n)                                                         \
+    do {                                                                             \
+        if(n > v_capacity(vector)) {                                                 \
+            void* p = malloc((n)*sizeof(*vector)+VECTOR_META_SIZE);                  \
+            if(p != NULL) vector = p+VECTOR_META_SIZE, v_meta(vector)->capacity = n; \
+        }                                                                            \
+     } while(0)
+
+/**
+ * Returns a reference to the element at position n in the vector.
+ * @param {vector} vector
+ * @param {size_t} n
+ * @return typename(*vector)
+ */
+#define v_at(vector, n) \
+    vector[n]
+
+/**
+ * Returns a reference to the first element in the vector.
+ * @param {vector} vector
+ * @return typename(*vector)
+ */
+#define v_front(vector) \
+    vector[0]
+
+/**
+ * Returns a reference to the last element in the vector.
+ * @param {vector} vector
+ * @return typename(*vector)
+ */
+#define v_back(vector) \
+    vector[v_size(vector)-1]
+
+/**
+ * Adds a new element at the end of the vector, after its current last element. The content of val is copied to the new element.
+ * @param {vector} vector
+ * @param {typename(*vector)} val
+ */
+#define v_push_back(vector, val)                                       \
+    if(v_size(vector) == v_capacity(vector)) v_grow(vector);           \
+    vector[v_size(vector)] = (val);                                    \
+    v_meta(vector)->size++;
+
+/**
+ * Removes the last element in the vector, effectively reducing the container size by one.
+ * @param {vector} vector
+ */
+#define v_pop_back(vector)  \
+    v_meta(vector)->size--;
+
+/**
+ * The vector is extended by inserting a new element before the element at given index, effectively increasing the container size by one.
+ * @param {vector} vector
+ * @param {size_t} index
+ * @param {typename(*vector)} val
+ */
+#define v_insert(vector, index, val) \
+    if(v_size(vector) == v_capacity(vector)) v_grow(vector); \
+    memmove(vector+(index)+1, vector+(index), v_size(vector)-(index)); \
+    vector[index] = (val);  
+/**
+ * Removes from the vector a range of elements ([from,to)).
+ * @param {vector} vector
+ * @param {size_t} from
+ * @param {size_t} to
+ */
+#define v_erase(vector, from, to)                                                    \
+   memcpy(vector+(from), vector+(to), (v_size(vector)-(to)+(from))*sizeof(*vector)); \
+   v_meta(vector)->size -= (to)-(from);
+
+/**
+ * Removes all elements from the vector (which are destroyed), leaving the container with a size of 0.
+ * @param {vector} vector
+ */
+#define v_clear(vector) \
+    memset(vector, 0, v_size(vector)*typeof(*vector)); \
+    vector.size = 0;
+
+/**
+ * Creates a copy of src_vector and stores it at dst_vector.
+ * @param {vector} dst_vector
+ * @param {vector} src_vector
+ */
+#define v_copy(dst_vector, src_vector)                                 \
+    dst_vector = malloc(v_raw_byte_size(src_vector))+VECTOR_META_SIZE; \
+    v_meta(dst_vector)->capacity = v_capacity(src_vector);             \
+    v_meta(dst_vector)->size = v_size(src_vector);                     \
+    memcpy(dst_vector, src_vector, v_size(src_vector)*sizeof(*src_vector));
